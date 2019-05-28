551260e1d4810faf8910af9c6e85cf7b52324cf4
Dream High <dreamhigh0822@outlook.com>
2019-05-29 02:04:03 +0900

    migration from 1 to 2 of apollo-server

--- server.js
+++ server.js
@@ -2,22 +2,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const db = require('./db');
-
+const { ApolloServer, gql } = require('apollo-server-express');
 const port = process.env.PORT || 9000;
 const app = express();
 
-const fs = require('fs')
-const typeDefs = fs.readFileSync('./schema.graphql',{encoding:'utf-8'})
-const resolvers = require('./resolvers')
-
-const {makeExecutableSchema} = require('graphql-tools')
-const schema = makeExecutableSchema({typeDefs, resolvers})
+const fs = require('fs');
+const typeDefs = fs.readFileSync('./schema.graphql',{encoding:'utf-8'});
+const resolvers = require('./resolvers');
+// const schema = makeExecutableSchema({typeDefs, resolvers})
+const server = new ApolloServer({ typeDefs, resolvers });
 
 app.use(cors(), bodyParser.json());
-
-const  {graphiqlExpress,graphqlExpress} = require('apollo-server-express')
-app.use('/graphql',graphqlExpress({schema}))
-app.use('/graphiql',graphiqlExpress({endpointURL:'/graphql'}))
+server.applyMiddleware({ app });
 
 app.listen(
    port, () => console.info(
